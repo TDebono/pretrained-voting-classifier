10a9132e3ee661fd22f373206e5052fa4ab86874
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-06-02 11:13:24 +0200

    Create voting_classifier.py

---
+++ voting_classifier.py
@@ -0,0 +1,37 @@
+class VotingClassifier(object):
+    """Stripped-down version of VotingClassifier that uses prefit estimators"""
+
+    def __init__(self, estimators, voting='hard', weights=None):
+        self.estimators = [e[1] for e in estimators]
+        self.named_estimators = dict(estimators)
+        self.voting = voting
+        self.weights = weights
+
+    def fit(self, X, y, sample_weight=None):
+        raise NotImplementedError
+
+    def predict(self, X):
+        """ Predict class labels for X.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and
+            n_features is the number of features.
+        Returns
+        ----------
+        maj : array-like, shape = [n_samples]
+            Predicted class labels.
+        """
+
+        check_is_fitted(self, 'estimators')
+        if self.voting == 'soft':
+            maj = np.argmax(self.predict_proba(X), axis=1)
+
+        else:  # 'hard' voting
+            predictions = self._predict(X)
+            maj = np.apply_along_axis(lambda x:
+                                      np.argmax(np.bincount(x,
+                                                            weights=self.weights)),
+                                      axis=1,
+                                      arr=predictions.astype('int'))
+        return maj
